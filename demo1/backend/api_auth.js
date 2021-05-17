7d8c63bc4ebfe419eb475d2f9445901c33223cee
codemobiles <chaiyasit.t@gmail.com>
2021-05-17 15:27:43 +0700

    1234

--- demo1/backend/api_auth.js
+++ demo1/backend/api_auth.js
@@ -1,14 +1,15 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const Users = require("./models/user_schema");
 
-router.post("/login", (req, res)=>{
-    res.json({result: "login ok", content: req.body})
-})
+router.post("/login", (req, res) => {
+  res.json({ result: "login ok", content: req.body });
+});
 
-router.post("/register", (req, res)=>{
-    const username = req.body.username
-    const password = req.body.password
-    res.json({result: "register ok", content: [username, password]})
-})
+router.post("/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  res.json({ result: "register ok", content: [username, password] });
+});
 
 module.exports = router;
