94bea2b5df80fb48659a3f3ab597ee211f63769f
codemobiles <chaiyasit.t@gmail.com>
2021-05-17 11:26:42 +0700

    1234

---
+++ demo1/backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+
+app.get("/login", (req, res)=>{
+    res.json({result: "login ok"})
+})
+
+app.get("/register", (req, res)=>{
+    res.json({result: "register ok"})
+})
+
+app.listen(3000, ()=>{
+    console.log("server is ready..")
+})
