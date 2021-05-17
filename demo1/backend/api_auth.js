d6f2d9f42db1c94ecfc4a282ee09689031a64b54
codemobiles <chaiyasit.t@gmail.com>
2021-05-17 15:59:15 +0700

    1234

--- demo1/backend/api_auth.js
+++ demo1/backend/api_auth.js
@@ -2,14 +2,14 @@ const express = require("express");
 const router = express.Router();
 const Users = require("./models/user_schema");
 
-router.post("/login", (req, res) => {
-  res.json({ result: "login ok", content: req.body });
+router.post("/login", async (req, res) => {
+  const doc = await Users.find({});
+  res.json(doc);
 });
 
 router.post("/register", async (req, res) => {
   try {
     const doc = await Users.create(req.body);
-    
     res.json({ result: "register ok", detail: doc });
   } catch (e) {
     res.json({ result: "register nok", detail: e });
