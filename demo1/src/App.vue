71c35fc7ee35c13e2929bf237e9b73af65206078
codemobiles <chaiyasit.t@gmail.com>
2021-05-18 11:19:06 +0700

    1234

--- demo1/src/App.vue
+++ demo1/src/App.vue
@@ -3,7 +3,7 @@
     <h1>My name is {{ myName }}</h1>
     <h1 v-on:click="onClickCountBtn">Count is {{ count }}</h1>
     <img alt="Vue logo" src="./assets/logo.png" />
-    <MyComponent title="lek"></MyComponent>
+    <MyComponent v-bind:title="count"></MyComponent>
   </div>
 </template>
 
